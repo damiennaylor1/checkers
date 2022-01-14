3239d7cc33a0440c929364c4d9a12be672be4b8c
damiennaylor1 <96264874+damiennaylor1@users.noreply.github.com>
2022-01-14 13:57:18 -0600

    Update checkers.java
    Removed choosing and put it in main file

--- checkers.java
+++ checkers.java
@@ -374,7 +374,7 @@ public class checkers {
                 break;
         }
     }
-    public void main() {
+    public int[] main(int[] receiveThis) {
     	
     	// Declare variables for running the game
         int turn = 0;
@@ -414,59 +414,7 @@ public class checkers {
             }
         }
         // Get input. Let user choose game mode and other options
-        System.out.println("Do you wish to enable free play? (no turns)");
-        String freeornot = scan.nextLine(); // Free mode selection
-        if (freeornot.toLowerCase().equals("yes")) {
-            free = 1;
-        } else {
-        	// Computer mode selection
-            System.out.println("Would you like to enable the computer opponent?");
-            String computerornot = scan.nextLine();
-            if (computerornot.toLowerCase().equals("yes")) {
-                robot = 1;
-            }
-            
-            // Coinflip for deciding which player starts first
-            System.out.println("Would you like to coinflip to decide who goes first, or choose?");
-            String CorC = scan.nextLine().toLowerCase();
-            if (CorC.equals("coinflip") || CorC.equals("coin flip") || CorC.equals("coin") || CorC.equals("flip")) {
-                // Do a coinflip to randomly select which player starts
-            	int chance = (int)(Math.random()*2+1);
-                switch(chance) {
-                    case 1:
-                        if (robot == 0) {
-                            System.out.println("It is heads. Player 1 goes first.");
-                        } else {
-                            System.out.println("It is heads. Computer goes first.");
-                            robotturn = 1;
-                        }
-                        turn=1;
-                        break;
-                    case 2:
-                        System.out.println("It is tails. Player 2 goes first.");
-                        turn=2;
-                        break;
-                    default:
-                    	// Backup in-case there is somehow an error
-                        System.out.println("ERROR: Coinflip is less than 1 or greater than 2");
-                        System.out.println("Player 2 is chosen due to this error.");
-                        break;
-                }
-            } else if (CorC.equals("choose") || CorC.equals("pick")) {
-            	// Let user manually decide which player goes first
-                Boolean chosen = false;
-                do{
-                    System.out.println("Type 1 or 2 to decide who goes first.");
-                    int chosep = scan.nextInt();
-                    if (chosep==1 || chosep==2) {
-                        turn=chosep;
-                        chosen=true;
-                    } else {
-                        System.out.println("INVALID NUMBER, MUST BE (1-2)");
-                    }
-                }while(chosen==false);
-            }
-        }
+        free = receiveThis[0]; robot = receiveThis[1]; turn = receiveThis[2]; robotturn = receiveThis[3];
         
         // Declare variables
         int turnCheck=0; 
@@ -1160,6 +1108,8 @@ public class checkers {
                 }
             }
         }
+        int[] returnThis = new int[1]; // Required to return value so returning empty int[].
+        return returnThis;
     }
     public String checkMoves(String incoming) {
         int cord1 = Integer.parseInt(String.valueOf(incoming.charAt(0)));
